cf6bed5b76592563f36a2bbde9ff340e1fdc8ba9
Greg Wessels <wesselsga@gmail.com>
2014-02-11 23:07:16 -0600

    removing raspi_config.sh

--- linux_config.sh
+++ linux_config.sh
@@ -4,11 +4,27 @@ in_dir="."
 out_dir="$in_dir/build"
 gyp_home="$in_dir/tools/gyp"
 
-"$gyp_home/gyp" reno.gyp \
---debug=all \
---depth=. \
--f make \
---generator-output="$out_dir" \
--Darch=x86
+platform=$(cat /etc/*-release | grep '^ID=' | sed 's/ID=//')
 
+echo $platform
+
+if [[ "$platform" == 'raspbian' ]]; then
+
+   "$gyp_home/gyp" reno.gyp \
+      --debug=all \
+      --depth=. \
+      -f make \
+      --generator-output="$out_dir" \
+      -Darch=arm \
+      -Ddevice=raspi
+else
+
+   "$gyp_home/gyp" reno.gyp \
+      --debug=all \
+      --depth=. \
+      -f make \
+      --generator-output="$out_dir" \
+      -Darch=x86
+
+fi
 
