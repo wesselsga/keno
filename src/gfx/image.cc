4bfdcbd495504e2c3bff2927c7ca6823ee15e4f1
Greg Wessels <wesselsga@gmail.com>
2014-03-09 00:32:53 +0000

    compiler errors on pi

--- src/gfx/image.cc
+++ src/gfx/image.cc
@@ -106,8 +106,8 @@ std::shared_ptr<Image> Image::create(const std::string& filename)
 
 	err = glGetError();
 
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+   //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+   //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);   
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
