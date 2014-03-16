b1e7b30526cbfd57b5bb9d2a49115558b13a9f18
Greg Wessels <wesselsga@gmail.com>
2014-03-16 15:29:09 -0500

    testing bash colors

--- make_depends.sh
+++ make_depends.sh
@@ -5,7 +5,7 @@ BUILD_DIR="$IN_DIR/build"
 GYP_HOME="$BUILD_DIR/gyp"
 
 mkdir -p "$BUILD_DIR"
-echo "IN_DIR=$IN_DIR"
+echo "$(tput setaf 6)IN_DIR=$IN_DIR$(tput sgr0)"
 echo "BUILD_DIR=$BUILD_DIR"
 
 # download gyp
@@ -14,8 +14,13 @@ echo "Downloading gyp ..."
 git clone "https://git.chromium.org/external/gyp.git" "$GYP_HOME"
 fi
 
+exit
+
 # what flavor of linux is this?
 DISTRIB=$(cat /etc/*-release | grep '^ID=' | sed 's/ID=//')
+if [ "$DISTRIB" == 'raspbian' ]; then
+   DISTRIB = 'raspi';
+fi
 echo "DISTRIB=$DISTRIB"
 
 # set the processor target
@@ -42,7 +47,7 @@ mkdir -p "$BUILD_DIR/include/uv"
 cp include/*.h "$BUILD_DIR/include/uv"
 fi
 
-# build free image
+# build freeimage
 if [ ! -d "$BUILD_DIR/third_party/freeimage" ]; then
 echo "Building freeimage ..."
 unzip -q "$IN_DIR/third_party/freeimage.zip" -d "$BUILD_DIR/third_party/"
@@ -53,7 +58,7 @@ mv Dist/libfreeimage.a "$BUILD_DIR/lib/$PLATFORM/release"
 # make install DESTDIR=$BUILD_DIR
 fi
 
-# build free type
+# build freetype
 if [ ! -d "$BUILD_DIR/third_party/freetype-2.4.11" ]; then
 echo "Building freetype ..."
 unzip -q "$IN_DIR/third_party/freetype-2.4.11.zip" -d "$BUILD_DIR/third_party/"
@@ -66,49 +71,48 @@ fi
 
 cd $IN_DIR
 
-# download and build v8
-V8_TARGET="ia32.release"
+# which v8 target are we building?
+V8_TARGET=""
 if [[ "$PLATFORM" == 'arm' ]]; then
-V8_TARGET="arm.release"
+   V8_TARGET="arm.release"
+else
+   V8_TARGET="ia32.release"
 fi
 
+# download v8 and its dependencies if we need to
 if [ ! -d "$BUILD_DIR/third_party/v8" ]; then
-echo "Building v8 $V8_TARGET ..."
-git clone https://github.com/v8/v8.git "$BUILD_DIR/third_party/v8"
-unzip -q "$IN_DIR/third_party/icu.zip" -d "$BUILD_DIR/third_party/v8/third_party/"
-git clone https://git.chromium.org/external/gyp.git "$BUILD_DIR/third_party/v8/build/gyp"
-cd "$BUILD_DIR/third_party/v8"
-make "$V8_TARGET"
-cp out/$V8_TARGET/obj.target/tools/gyp/*.a "$BUILD_DIR/lib/$PLATFORM/release/"
-cp out/$V8_TARGET/obj.target/third_party/icu/*.a "$BUILD_DIR/lib/$PLATFORM/release/"
-mkdir "$BUILD_DIR/include/v8"
-cp include/*.h "$BUILD_DIR/include/v8/"
+   echo "Download v8  ..."
+   git clone https://github.com/v8/v8.git "$BUILD_DIR/third_party/v8"
+   unzip -q "$IN_DIR/third_party/icu.zip" -d "$BUILD_DIR/third_party/v8/third_party/"
+   git clone https://git.chromium.org/external/gyp.git "$BUILD_DIR/third_party/v8/build/gyp"
+else
+   V8_TARGET=""
+fi
+
+# on raspi; we need to force arm6
+if [ "$DISTRIB" == 'raspi' ] && [ -n V8_TARGET ]; then
+   V8_TARGET = "$V8_TARGET arm7=false vfp3=off hardfp=on" 
+fi
+
+# actually build v8 if we have a target options
+if [ -n $V8_TARGET ]; then 
+   echo "Building v8 $V8_TARGET ..."
+   cd "$BUILD_DIR/third_party/v8"
+   make "$V8_TARGET"
+   cp out/$V8_TARGET/obj.target/tools/gyp/*.a "$BUILD_DIR/lib/$PLATFORM/release/"
+   cp out/$V8_TARGET/obj.target/third_party/icu/*.a "$BUILD_DIR/lib/$PLATFORM/release/"
+   mkdir "$BUILD_DIR/include/v8"
+   cp include/*.h "$BUILD_DIR/include/v8/"
 fi
 
 cd $IN_DIR
 
-if [[ "$DISTRIB" == 'raspbian' ]]; then
+# finally; generate our projects
+"$GYP_HOME/gyp" reno.gyp \
+   --debug=all \
+   --depth=. \
+   -f make \
+   --generator-output="$BUILD_DIR" \
+   -Darch=$PLATFORM \
+   -Ddistrib=$DISTRIB
 
-   "$GYP_HOME/gyp" reno.gyp \
-      --debug=all \
-      --depth=. \
-      -f make \
-      --generator-output="$BUILD_DIR" \
-      -Darch=$PLATFORM \
-      -Ddistrib=raspi
-else
-
-   "$GYP_HOME/gyp" reno.gyp \
-      --debug=all \
-      --depth=. \
-      -f make \
-      --generator-output="$BUILD_DIR" \
-      -Darch=$PLATFORM
-
-fi
-
-
-#echo "building v8..."
-#tar -xzvf "$src_dir/v8.tar.gz" --directory="$build_dir/"
-#cd "$build_dir/v8"
-#make arm.release armv7=false vfp3=off hardfp=on
